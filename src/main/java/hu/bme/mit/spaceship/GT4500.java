113616b7024fb2f98f20e6070df67fa1c6a869a9
ETomi24 <tamas.ernyei@gmail.com>
2022-04-01 13:19:53 +0200

    Branch-a valtoztatas

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -82,7 +82,7 @@ public class GT4500 implements SpaceShip {
           firingSuccess = primaryTorpedoStore.fire(1);
           firingSuccess = secondaryTorpedoStore.fire(1);
         }
-
+        //valtoztatas
         break;
     }
 
